157ec3fcc93aeae9631627e0fddd0afc834c75cb
farhan <farhan.azizi.gh@gmail.com>
2026-02-22 17:23:43 +0330

    feat: debug_toolbar.middleware.DebugToolbarMiddleware added to MIDDLEWARE

--- config/settings.py
+++ config/settings.py
@@ -65,6 +65,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # package 
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
