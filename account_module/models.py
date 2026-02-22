8ce228c20760a5f42f22c617e3e772743d1f931f
farhan <farhan.azizi.gh@gmail.com>
2026-02-22 17:14:04 +0330

    feat: create class User for auth

--- account_module/models.py
+++ account_module/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class User(AbstractUser):
+    phone = models.CharField(max_length=15, verbose_name='شماره تلفن', null=True, blank=True)
+    address = models.TextField(null=True, blank=True, verbose_name='آدرس')
+    avatra = models.ImageField(null=True, blank=True, upload_to='users/avatar', verbose_name='عکس')
