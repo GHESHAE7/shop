9cea5a7a59a63c96e92647c017fd7242d3541487
farhan <farhan.azizi.gh@gmail.com>
2026-02-23 12:02:17 +0330

    feat: config urls and views for register page

--- account_module/views.py
+++ account_module/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+
+
+
+class RegisterView(View):
+    def get(self, request):
+        context = {}
+        return render(request, 'account_module/register.html', context)
+    
+    def post(self, request):
+        pass
