991856fc3a5580580c2f8c02ca0015a96655621e
farhan <farhan.azizi.gh@gmail.com>
2026-02-24 14:23:24 +0330

    feat and fix: update profile and views bug

--- home_module/views.py
+++ home_module/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from account_module.models import User
 # Create your views here.
 
 
@@ -13,8 +14,16 @@ class HomeView(View):
     
     
 def header_component(request):
-    context = {}
-    return render(request, "component_partial/header_component.html", context)
+    if request.user.is_authenticated:
+        user_id = request.user.id
+        current_user = User.objects.filter(id=user_id, is_active=True).first()
+        context = {
+            'user': current_user
+        }
+        return render(request, "component_partial/header_component.html", context)
+    else:
+        context = {}
+        return render(request, "component_partial/header_component.html", context)
 
 
 def footer_component(request):
