6b04e17e202e97b5d3427c03e31756e103f01a7e
farhan <farhan.azizi.gh@gmail.com>
2026-02-24 13:31:03 +0330

    fix: after create account redirect to login page

--- account_module/views.py
+++ account_module/views.py
@@ -22,10 +22,9 @@ class RegisterView(View):
             n_password = form.cleaned_data['password']
             new_user.set_password(n_password)
             new_user.save()
-            print('create')
             # messages.success(request, 'حساب کاربری شما با موفقیت ساخته شد')
             print('new_user save')
-            return redirect(reverse('account_module:register_page'))
+            return redirect(reverse('account_module:login_page'))
         # messages.error(request, 'کاربری با این مشخصات وجود دارد')
         print(form.errors)
         return redirect(reverse('account_module:register_page'))
