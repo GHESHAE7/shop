cfa3fe4b6f11f0a6c5f22be60c1c76bff30e3108
farhan <farhan.azizi.gh@gmail.com>
2026-02-25 12:28:09 +0330

    feat: added field email_active_code for user and activate

--- account_module/models.py
+++ account_module/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.crypto import get_random_string
 # Create your models here.
 
 
@@ -7,3 +8,4 @@ class User(AbstractUser):
     phone = models.CharField(max_length=15, verbose_name='شماره تلفن', null=True, blank=True)
     address = models.TextField(null=True, blank=True, verbose_name='آدرس')
     avatra = models.ImageField(null=True, blank=True, upload_to='users/avatar', verbose_name='عکس')
+    email_active_code = models.CharField(max_length=126, null=True, blank=True, default=get_random_string(126))
