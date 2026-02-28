c6f8a4504e0af235ba22fedca34ba211ad28335a
farhan <farhan.azizi.gh@gmail.com>
2026-02-28 23:44:34 +0330

    feat and fix: fix account_module/migrations and deleted defualt email_active_ode field User model

--- account_module/models.py
+++ account_module/models.py
@@ -8,4 +8,4 @@ class User(AbstractUser):
     phone = models.CharField(max_length=15, verbose_name='شماره تلفن', null=True, blank=True)
     address = models.TextField(null=True, blank=True, verbose_name='آدرس')
     avatra = models.ImageField(null=True, blank=True, upload_to='users/avatar', verbose_name='عکس')
-    email_active_code = models.CharField(max_length=126, null=True, blank=True, default=get_random_string(126))
+    email_active_code = models.CharField(max_length=126, null=True, blank=True)
