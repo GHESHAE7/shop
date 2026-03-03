db0935e432eebc15836e457b7f2763cf7a2e5c76
farhan <farhan.azizi.gh@gmail.com>
2026-03-03 15:16:35 +0330

    feat: added humanize in INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     # package
     "django_render_partial",
     "sorl.thumbnail",
