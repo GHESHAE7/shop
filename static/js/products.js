eea573435d2d9bdc49067fb457f38f78351e8c28
farhan <farhan.azizi.gh@gmail.com>
2026-03-03 15:24:15 +0330

    feat: create product.js for product page

---
+++ static/js/products.js
@@ -0,0 +1,14 @@
+// document.addEventListener('DOMContentLoaded', order_by);
+
+
+// function order_by(){
+//     document.getElementById('selectProductCatagory').addEventListener('click', function(){
+//         console.log('hello');
+        
+//     })
+// };
+
+
+// function say(){
+//     console.log('hello');
+// };
