8edef105f56474d9ac9e9f8a6b56b2608e822ff4
farhan <farhan.azizi.gh@gmail.com>
2026-03-04 20:01:21 +0330

    fix: rename url

--- product_module/urls.py
+++ product_module/urls.py
@@ -6,7 +6,7 @@ app_name = 'product_module'
 
 urlpatterns = [
     path('', ProductsListView.as_view(), name='products_page'),
-    path('category/<category_url>', ProductsListView.as_view(), name='products_by_category'),
-    path('brand/<brand_url>', ProductsListView.as_view(), name='products_by_brand'),
+    path('category/<category_url>', ProductsListView.as_view(), name='products_by_category_page'),
+    path('brand/<brand_url>', ProductsListView.as_view(), name='products_by_brand_page'),
     path('discount', ProductsListView.as_view(), name='products_discount_page'),
 ]
