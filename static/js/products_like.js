ec502ab2ec87742ea074cf8408d9fcae5e37b7d1
farhan <farhan.azizi.gh@gmail.com>
2026-03-05 17:08:27 +0330

    feat: create function delete_product_to_likes for delete product in likes

---
+++ static/js/products_like.js
@@ -0,0 +1,7 @@
+function delete_product_to_likes(id_like_product){
+    let csrf_token = document.getElementById('#csrf_t');
+    $.post('http://127.0.0.1:8000/likes/delete', {like_product_id:id_like_product, csrfmiddlewaretoken:csrf_token.value}, function(res){
+        console.log(res);
+        window.location.reload();
+    });
+}
