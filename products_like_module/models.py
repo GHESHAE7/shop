d66465dfa908415d0d84b66237a038eb950d8fec
farhan <farhan.azizi.gh@gmail.com>
2026-03-05 13:41:47 +0330

    feat: create LikesProduct model

--- products_like_module/models.py
+++ products_like_module/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from account_module.models import User
+from product_module.models import Product
 # Create your models here.
+
+
+class LikesProduct(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, verbose_name='کاربر')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False, blank=False, verbose_name='محصول')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='آخرین آپدیت')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    is_active = models.BooleanField(default=True, null=False, verbose_name='فعال / غیر فعال')
+    
+    def __str__(self):
+        return self.user.username if self.user.username else self.user.email
