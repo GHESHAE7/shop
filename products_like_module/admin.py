a27bd3a460d11e1b52fc56848673adaf601dff9a
farhan <farhan.azizi.gh@gmail.com>
2026-03-05 13:44:40 +0330

    feat: setup admin.py for products_like_module/models.py

--- products_like_module/admin.py
+++ products_like_module/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import LikesProduct
 # Register your models here.
+
+
+@admin.register(LikesProduct)
+class LikesProductAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    list_display = ('user__email' ,'is_active', 'updated_at', 'created_at',)
+    list_filter = ('is_active', 'created_at', 'updated_at',)
+    list_editable = ('is_active',)
+    readonly_fields = ('created_at', 'updated_at',)
+    search_fields = ('products',)
