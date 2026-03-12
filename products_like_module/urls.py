a33aa273a65013c34838afa5abb3a7a46b264cc8
farhan <farhan.azizi.gh@gmail.com>
2026-03-12 17:58:36 +0330

    added app_name to urls

--- products_like_module/urls.py
+++ products_like_module/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import LikeProductsView, delete_product_likes
 
 
+app_name = 'products_like_module'
+
 urlpatterns = [
     path('', LikeProductsView.as_view(), name='like_products_page'),
     path('delete', delete_product_likes, name='delete_like_products'),
