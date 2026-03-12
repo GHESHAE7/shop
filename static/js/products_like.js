c1cf16042c876154104b1bdb791e29e7a11647e4
farhan <farhan.azizi.gh@gmail.com>
2026-03-12 17:37:58 +0330

    feat: move functions add_product_to_likes to products_like.js

--- static/js/products_like.js
+++ static/js/products_like.js
@@ -5,3 +5,10 @@ function delete_product_to_likes(id_like_product){
         window.location.reload();
     });
 }
+
+function add_product_to_likes(id_product){
+    let csrf_token = document.getElementById('#csrf_t');
+    $.post('http://127.0.0.1:8000/likes/', {product_id:id_product, csrfmiddlewaretoken:csrf_token.value}, function(res){console.log(res);
+    });
+    
+}
