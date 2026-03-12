3e9afda198a74958947d9fe89ebc9d5fab30b47e
farhan <farhan.azizi.gh@gmail.com>
2026-03-12 16:45:24 +0330

    feat: added related products section to inclusion

--- product_module/templatetags/products_tags.py
+++ product_module/templatetags/products_tags.py
@@ -1,4 +1,8 @@
 from django import template
+from product_module.models import Product
+from django.db.models import Max, Q
+from django.utils import timezone
+from datetime import timedelta
 
 
 
@@ -9,3 +13,16 @@ register = template.Library()
 def res_discount(value, price):
     result = price - ((price / 100) * value)
     return int(result)
+
+
+
+@register.inclusion_tag('product_module/inclusion/related_products.html')
+def related_products_for_detail_page(brand):
+    products = Product.objects.filter(is_active=True, brand__url=brand).annotate(discount=Max('product_variant__discount')).order_by('-created_at')
+    old_time = timezone.now() - timedelta(7)
+
+    return {
+        'products': products,
+        'old_time': old_time,
+        'brand': brand,
+    }
