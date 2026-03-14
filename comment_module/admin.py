fb1afcb075d9ac367ad72823ed25bf26a3ceead2
farhan <farhan.azizi.gh@gmail.com>
2026-03-14 11:02:50 +0330

    feat: config admins for Comment model

--- comment_module/admin.py
+++ comment_module/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Comment
 # Register your models here.
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('product__name', 'user__email', 'is_active', 'updated_at', 'created_at',)
+    list_filter = ('product', 'is_active', 'updated_at', 'created_at',)
+    list_editable = ('is_active',)
+    readonly_fields = ('created_at', 'updated_at',)
+    date_hierarchy = 'created_at'
+    search_fields = ('product', 'user', 'message')
