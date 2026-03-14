200031ce18e1389d3a59fa03db0a82f59dce14e3
farhan <farhan.azizi.gh@gmail.com>
2026-03-14 11:42:54 +0330

    feat: create view for show comment and setup single_comment.html

--- comment_module/views.py
+++ comment_module/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from comment_module.models import Comment
 
 # Create your views here.
+
+
+
+def comments_product(request, product_id):
+    comments = Comment.objects.filter(is_active=True, product_id=product_id).order_by('-created_at')
+    context = {
+        'comments': comments
+    }
+    
+    return render(request, 'comment_module/component_partial/single_comment.html', context)
