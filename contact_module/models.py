de5942bc7b67acfd40d03139d8da75be1402150e
farhan <farhan.azizi.gh@gmail.com>
2026-03-16 16:27:44 +0330

    feat: create ContactUs model for contact

--- contact_module/models.py
+++ contact_module/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class ContactUs(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='نام')
+    email = models.CharField(max_length=255, null=False, blank=False, verbose_name='ایمیل')
+    subject = models.CharField(max_length=255, null=False, blank=False, verbose_name='موصوغ')
+    message = models.TextField(null=False, blank=False, verbose_name='پیام')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='آخرین آپدیت')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    is_active = models.BooleanField(default=True, null=False, verbose_name='فعال / غیر فعال')
+    is_read_by_admin = models.BooleanField(default=False, verbose_name='خوانده شده توسط ادمین')
+    
+    
+    def __str__(self):
+        return self.name + '/' + self.email
