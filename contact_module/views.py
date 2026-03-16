a6047bbeb983d7cef821609fbe4c9829aae10b82
farhan <farhan.azizi.gh@gmail.com>
2026-03-16 17:03:15 +0330

    feat: added all subjects in context and configed to view

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from .models import SubjectContact
 # Create your views here.
 
 
 class ContactView(View):
     def get(self, request):
-        context = {}
+        subjects = SubjectContact.objects.filter(is_active=True)
+        context = {
+            'subjects': subjects,
+        }
         return render(request, 'contact_module/contact_us.html', context)
     
     
