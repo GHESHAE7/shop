f7a50b056b053106bde5d7dc30a2d3b812fd15bf
farhan <farhan.azizi.gh@gmail.com>
2026-03-16 16:38:44 +0330

    feat: setup admin for contact

--- contact_module/admin.py
+++ contact_module/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import ContactUs
 # Register your models here.
+
+
+@admin.register(ContactUs)
+class ContactUsAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    list_display = ('name', 'email', 'subject', 'is_read_by_admin', 'is_active', 'updated_at', 'created_at',)
+    list_filter = ('subject', 'is_read_by_admin', 'is_active', 'created_at',)
+    list_editable = ('is_active',)
+    readonly_fields = ('created_at', 'updated_at',)
+    search_fields = ('subject', 'email', 'name',)
