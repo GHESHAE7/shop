81382b1329ad50e5e62b85f0f525fcb3523ab107
farhan <farhan.azizi.gh@gmail.com>
2026-03-17 09:37:59 +0330

    feat: setup admins for support

--- support_module/admin.py
+++ support_module/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import Support, SupportCategory
 # Register your models here.
+
+
+@admin.register(Support)
+class SupportAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    list_display = ('name', 'is_active', 'updated_at', 'created_at',)
+    list_filter = ('is_active', 'created_at',)
+    list_editable = ('is_active',)
+    readonly_fields = ('created_at', 'updated_at',)
+    search_fields = ('category', 'name',)
+    
+    
+
+@admin.register(SupportCategory)
+class SupportCategoryAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    list_display = ('name', 'is_active', 'updated_at', 'created_at',)
+    list_filter = ('is_active', 'created_at',)
+    list_editable = ('is_active',)
+    readonly_fields = ('created_at', 'updated_at',)
+    search_fields = ('name',)
