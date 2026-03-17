46a5ddc21c04330a8789836f2284ed80a3fdcf8d
farhan <farhan.azizi.gh@gmail.com>
2026-03-17 09:49:09 +0330

    feat: git added url for support page and create & setup view

--- support_module/views.py
+++ support_module/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+
 
 # Create your views here.
+
+
+class SupportView(TemplateView):
+    template_name = 'support_module/support.html'
