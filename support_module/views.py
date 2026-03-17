a437be018514846a37be27f2f542c460f53e3830
farhan <farhan.azizi.gh@gmail.com>
2026-03-17 10:18:28 +0330

    feat: configed view and support.html for support page

--- support_module/views.py
+++ support_module/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from .models import SupportCategory
 
 
 
@@ -8,3 +9,8 @@ from django.views.generic.base import TemplateView
 
 class SupportView(TemplateView):
     template_name = 'support_module/support.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories_support'] = SupportCategory.objects.filter(is_active=True).prefetch_related('supports')
+        return context
