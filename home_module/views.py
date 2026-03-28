75358927876c90a05878c84b74061c6d3eaab3c5
farhan <farhan.azizi.gh@gmail.com>
2026-03-28 16:28:31 +0330

    feat: some sections is done

--- home_module/views.py
+++ home_module/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.views import View
 from account_module.models import User
+from product_module.models import Brand, Category, Product
+from django.db.models import Count, Max
+from django.utils import timezone
+from datetime import timedelta
 # Create your views here.
 
 
 class HomeView(View):
     def get(self, request):
-        context = {}
+        brands = Brand.objects.filter(is_active=True).values('name', 'url', 'image')
+        categories = Category.objects.filter(is_active=True).annotate(count_products=Count('products_category')).values('name', 'url', 'image', 'count_products')
+        products_discount = Product.objects.filter(is_active=True, product_variant__discount__isnull=False).annotate(discount=Max('product_variant__discount'))[:8]
+        old_time = timezone.now() - timedelta(days=7)
+        products_new = Product.objects.filter(is_active=True, created_at__gte=old_time).annotate(discount=Max('product_variant__discount'))[:8]
+        context = {
+            'brands': brands,
+            'categories': categories,
+            'products_discount': products_discount,
+            'products_new': products_new,
+        }
         return render(request, 'home_module/home.html', context)
     
     def post(self, request):
