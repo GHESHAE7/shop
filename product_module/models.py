6eb56f926bfa3ca3aae9b7d387c85b66917fb7ad
farhan <farhan.azizi.gh@gmail.com>
2026-03-28 15:24:56 +0330

    feat: added related_name for category field in product model

--- product_module/models.py
+++ product_module/models.py
@@ -38,7 +38,7 @@ class Product(models.Model):
         both = 'both', _('Both')
     
     name = models.CharField(max_length=255, null=False, blank=False, verbose_name='نام محصول')
-    category = models.ManyToManyField(Category, null=False, verbose_name='دسته بندی محصول')
+    category = models.ManyToManyField(Category, null=False, verbose_name='دسته بندی محصول', related_name="products_category")
     brand = models.ForeignKey(Brand, null=True, on_delete=models.SET_NULL, verbose_name='برند محصول')
     price = models.BigIntegerField(null=False, blank=False, verbose_name='قیمت محصول')
     description = models.TextField(verbose_name='توضیحات محصول', null=False, blank=False)
