0d669d6ec680a184b68b6a3ef3e92af290e0bb35
farhan <farhan.azizi.gh@gmail.com>
2026-03-29 18:08:49 +0330

    feat: setup admin for Baner

--- site_setting_module/admin.py
+++ site_setting_module/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Baner
 
 # Register your models here.
+
+
+@admin.register(Baner)
+class BanerAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    list_display = ('title', 'url_btn', 'is_active', 'updated_at', 'created_at',)
+    list_filter = ('is_active', 'created_at', 'updated_at',)
+    list_editable = ('is_active',)
+    readonly_fields = ('created_at', 'updated_at',)
+    search_fields = ('title', 'description', 'url_btn', 'text_in_btn')
