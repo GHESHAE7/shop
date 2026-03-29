9da02aef88b5da035d856c43be586131cf939e89
farhan <farhan.azizi.gh@gmail.com>
2026-03-29 18:06:31 +0330

    feat: added upload_to in feild image for baner model

--- site_setting_module/models.py
+++ site_setting_module/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Baner(models.Model):
     title = models.CharField(max_length=60, null=False, blank=False, verbose_name="عنوان")
     description = models.CharField(max_length=100, null=True, blank=True, verbose_name="توضیحات")
-    image = models.ImageField(max_length=500, null=False, blank=False, verbose_name="عکس")
+    image = models.ImageField(max_length=500, null=False, blank=False, verbose_name="عکس", upload_to='baner/image')
     text_in_btn = models.CharField(max_length=100, null=False, blank=True, verbose_name='متن داخل دکمه')
     url_btn = models.URLField(max_length=600, null=False, blank=False, verbose_name='url دکمه')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='آخرین آپدیت')
