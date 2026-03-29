72a69d96be2d6414e222bb194bf3d54fc1b658cf
farhan <farhan.azizi.gh@gmail.com>
2026-03-29 18:30:02 +0330

    feat: config home view and home.html for show baners

--- home_module/views.py
+++ home_module/views.py
@@ -5,6 +5,7 @@ from product_module.models import Brand, Category, Product
 from django.db.models import Count, Max
 from django.utils import timezone
 from datetime import timedelta
+from site_setting_module.models import Baner
 # Create your views here.
 
 
@@ -15,11 +16,13 @@ class HomeView(View):
         products_discount = Product.objects.filter(is_active=True, product_variant__discount__isnull=False).annotate(discount=Max('product_variant__discount'))[:8]
         old_time = timezone.now() - timedelta(days=7)
         products_new = Product.objects.filter(is_active=True, created_at__gte=old_time).annotate(discount=Max('product_variant__discount'))[:8]
+        baners = Baner.objects.filter(is_active=True).order_by('-created_at')[:3]
         context = {
             'brands': brands,
             'categories': categories,
             'products_discount': products_discount,
             'products_new': products_new,
+            'baners': baners,
         }
         return render(request, 'home_module/home.html', context)
     
