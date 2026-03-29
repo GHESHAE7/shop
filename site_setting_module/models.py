1a266d0be5ba301b912ddc068a548b24b0371967
farhan <farhan.azizi.gh@gmail.com>
2026-03-29 18:03:21 +0330

    feat: create Baner models for baners site

--- site_setting_module/models.py
+++ site_setting_module/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Baner(models.Model):
+    title = models.CharField(max_length=60, null=False, blank=False, verbose_name="عنوان")
+    description = models.CharField(max_length=100, null=True, blank=True, verbose_name="توضیحات")
+    image = models.ImageField(max_length=500, null=False, blank=False, verbose_name="عکس")
+    text_in_btn = models.CharField(max_length=100, null=False, blank=True, verbose_name='متن داخل دکمه')
+    url_btn = models.URLField(max_length=600, null=False, blank=False, verbose_name='url دکمه')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='آخرین آپدیت')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    is_active = models.BooleanField(default=True, null=False, verbose_name='فعال / غیر فعال')
+    
+    def __str__(self):
+        return self.title
