721d67f96fbd385fc0137ee849fb0cf0fffb65ed
farhan <farhan.azizi.gh@gmail.com>
2026-03-31 10:35:10 +0330

    feat: added field rating for comment

--- comment_module/models.py
+++ comment_module/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from product_module.models import Product
 from account_module.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -9,6 +10,7 @@ class Comment(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False, blank=False, verbose_name='محصول')
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, verbose_name='کاربر')
     message = models.TextField(null=False, blank=False, verbose_name='متن کامنت')
+    rating = models.IntegerField(null=False, blank=False, validators=[MaxValueValidator(5), MinValueValidator(1)], default=0)
     updated_at = models.DateTimeField(auto_now=True, verbose_name='آخرین آپدیت')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
     is_active = models.BooleanField(default=True, null=False, verbose_name='فعال / غیر فعال')
