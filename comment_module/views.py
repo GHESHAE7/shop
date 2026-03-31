8303ab96cabf669211eaa2e7221659f04c433e26
farhan <farhan.azizi.gh@gmail.com>
2026-03-31 11:55:37 +0330

    feat: config and edite js and view for send commend

--- comment_module/views.py
+++ comment_module/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from comment_module.models import Comment
 from django.http import JsonResponse
 from product_module.models import Product
-
+from django.db.models import Avg
 # Create your views here.
 
 
@@ -23,10 +23,18 @@ def add_commnet(request):
             product_id = request.POST.get('product_id')
             current_product = Product.objects.filter(is_active=True, id=product_id).first()
             if current_product:
-                message = request.POST.get('message')
-                new_comment = Comment(is_active=True, user_id=request.user.id, product_id=current_product.id, message=message)
-                new_comment.save()
-                return comments_product(request=request, product_id=product_id)
+                rating = request.POST.get('rating')
+                if int(rating) < 0 or int(rating) > 5:
+                    return JsonResponse({
+                        'status': 200,
+                        'message': 'rating gt 5 or rating lt 0'
+                    })
+                else:
+                    message = request.POST.get('message')
+                    new_comment = Comment(is_active=True, user_id=request.user.id, product_id=current_product.id, message=message, rating=int(rating))
+                    new_comment.save()
+                    get_rating = Comment.objects.filter(is_active=True, product_id=current_product.id).aggregate(Avg('rating'))['rating__avg'] or 0
+                    return comments_product(request=request, product_id=product_id)
             else:
                 return JsonResponse({
                     'status': 200,
--- static/js/comments.js
+++ static/js/comments.js
@@ -1,13 +1,19 @@
 function send_comment(product_id){
-    let csrf = document.getElementById('#csrf_t')
-    let comment = document.getElementById('comments').value
-    if (comment == ""){
-        console.log('باید متن کامنت پر شود');
+    let csrf = document.getElementById('#csrf_t');
+    let comment = document.getElementById('comments').value;
+    let rating = document.getElementById('comment_rating');
+    if(rating.value < 0 || rating.value > 5 || rating.value == '' || isFinite(rating.value) || isNaN(parseFloat(rating.value))){
+        console.log('نمیتونی بزرگ تر از 5 یا کوچک تر از 0 انتخاب کنی یا خالی وارد کنی');
     }else{
-        $.post('http://127.0.0.1:8000/comment/add', {product_id:product_id, csrfmiddlewaretoken:csrf.value, message:comment}, function(res){
-            document.getElementById('list_comment').innerHTML = res;
-            document.getElementById('comments').value = null;
-            // add alert
-        });
+        if (comment == ""){
+            console.log('باید متن کامنت پر شود');
+        }else{
+            $.post('http://127.0.0.1:8000/comment/add', {product_id:product_id, csrfmiddlewaretoken:csrf.value, message:comment, rating:rating.value}, function(res){
+                document.getElementById('list_comment').innerHTML = res;
+                document.getElementById('comments').value = null;
+                document.getElementById('comment_rating').value = null;
+                // add alert
+            });
+        }
     }
 }
