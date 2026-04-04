70810d269656db4ad62d98935dc0b8e00ca79508
farhan <farhan.azizi.gh@gmail.com>
2026-04-04 11:48:57 +0330

    feat: config admins for Order and OrderItem models

--- cart_module/admin.py
+++ cart_module/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from cart_module.models import Order, OrderItem
 # Register your models here.
+
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_number','user', 'status', 'is_active', 'total_price', 'updated_at', 'created_at',)
+    list_filter = ('order_number', 'status', 'is_active', 'updated_at', 'created_at',)
+    search_fields = ('user','order_number', 'status',)
+    readonly_fields = ('created_at', 'updated_at', 'total_price', 'order_number',)
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'product_variant', 'count', 'is_active', 'updated_at', 'created_at',)
+    list_filter = ('order', 'product', 'count', 'product_variant', 'is_active', 'updated_at', 'created_at',)
+    search_fields = ('product__product_name', 'order__order_number',)
+    readonly_fields = ('created_at', 'updated_at', 'price', 'product',)
