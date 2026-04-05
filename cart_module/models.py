e34cd77f43e9595a36ad9b7d4a2570e36c378237
farhan <farhan.azizi.gh@gmail.com>
2026-04-05 19:44:37 +0330

    feat: added default for status field

--- cart_module/models.py
+++ cart_module/models.py
@@ -19,7 +19,7 @@ class Order(models.Model):
         
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     order_number = models.IntegerField()
-    status = models.CharField(max_length=150, choices=status_choices)
+    status = models.CharField(max_length=150, choices=status_choices, default=status_choices.CART)
     total_price = models.DecimalField(max_digits=20, decimal_places=2, null=True)
     updated_at = models.DateTimeField(auto_now=True, verbose_name='آخرین آپدیت')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
