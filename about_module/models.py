b53865f152bec2bf8cc3297fdc1b941ab17d5d3d
farhan <farhan.azizi.gh@gmail.com>
2026-04-09 18:43:23 +0330

    created About model for about page

--- about_module/models.py
+++ about_module/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class About(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='نام درباره ما')
+    description = models.TextField(null=False, blank=False, verbose_name='متن درباره ما')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='آخرین آپدیت')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    is_active = models.BooleanField(default=True, null=False, verbose_name='فعال / غیر فعال')
+    
+    
+    def __str__(self):
+        return self.name
