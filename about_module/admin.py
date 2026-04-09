ba80c8668f06d0494d46471934a925dc0f8d980a
farhan <farhan.azizi.gh@gmail.com>
2026-04-09 18:45:29 +0330

    feat: configured admin for About model

--- about_module/admin.py
+++ about_module/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import About
 # Register your models here.
+
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active', 'updated_at', 'created_at',)
+    list_filter = ('created_at', 'updated_at', 'is_active', 'name')
+    list_editable = ('is_active',)
+    readonly_fields = ('updated_at', 'created_at',)
