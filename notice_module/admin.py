da1d3a10d3e9a9be12612290aa548cfdd9a4fa83
farhan <farhan.azizi.gh@gmail.com>
2026-04-10 19:22:18 +0330

    feat: created Notice model

--- notice_module/admin.py
+++ notice_module/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Notice
 # Register your models here.
+
+
+# @admin.register(Notice)
+# class NoticeAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'icon', 'is_active', 'updated_at', 'created_at',)
+#     list_filter = ('created_at', 'updated_at', 'is_active', 'icon')
+#     list_editable = ('is_active', 'icon')
+#     readonly_fields = ('updated_at', 'created_at',)
--- notice_module/models.py
+++ notice_module/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Notice(models.Model):
+    title = models.CharField(max_length=500, null=True, blank=True, verbose_name='هدر')
+    message = models.TextField(null=False, blank=False, verbose_name='متن اعلان')
+    icon = models.CharField(null=True, blank=True, default='fa-bell', verbose_name='ایکون اعلان')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='آخرین آپدیت')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    is_active = models.BooleanField(default=True, null=False, verbose_name='فعال / غیر فعال')
+    
+    
+    def __str__(self):
+        return self.title
