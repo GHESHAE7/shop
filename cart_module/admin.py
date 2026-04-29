26dd30a51f68abf6e15c00462a87e8c1c1bd18ce
farhan <farhan.azizi.gh@gmail.com>
2026-04-29 11:17:47 +0330

    feat: setup admin for DiscountCode, UserDiscountUsage models

--- cart_module/admin.py
+++ cart_module/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from cart_module.models import Order, OrderItem
+from cart_module.models import Order, OrderItem, DiscountCode, UserDiscountUsage
 # Register your models here.
 
 
@@ -12,9 +12,28 @@ class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at', 'updated_at', 'total_price',)
 
 
+
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
     list_display = ('order', 'product', 'product_variant', 'count', 'is_active', 'updated_at', 'created_at',)
     list_filter = ('order', 'product', 'count', 'product_variant', 'is_active', 'updated_at', 'created_at',)
     search_fields = ('product__product_name', 'order__id',)
     readonly_fields = ('created_at', 'updated_at', 'price', 'product',)
+
+
+
+@admin.register(DiscountCode)
+class DiscountCodeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'percent', 'amount', 'expires_at', 'max_uses', 'is_active','updated_at', 'created_at')
+    list_filter = ('percent', 'amount', 'expires_at', 'max_uses', 'is_active', 'updated_at', 'created_at',)
+    search_fields = ('code', 'percent', 'amount')
+    readonly_fields = ('created_at', 'updated_at',)
+
+
+
+@admin.register(UserDiscountUsage)
+class UserDiscountUsageAdmin(admin.ModelAdmin):
+    list_display = ('user__username', 'discount_code', 'status_usage', 'discount_amount_applied', 'is_active', 'updated_at', 'created_at',)
+    list_filter = ('user__username', 'discount_code', 'status_usage', 'is_active', 'updated_at', 'created_at',)
+    search_fields = ('user__username', 'discount_code', 'discount_amount_applied')
+    readonly_fields = ('created_at', 'updated_at', 'discount_amount_applied',)
