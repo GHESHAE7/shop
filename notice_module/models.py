17f7f0ba3c46e380c0458ed10222c60b2eaa360c
farhan <farhan.azizi.gh@gmail.com>
2026-05-07 11:12:57 +0330

    feat: aaded user feild for Notice model

--- notice_module/models.py
+++ notice_module/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from account_module.models import User
 
 # Create your models here.
 
@@ -7,6 +8,7 @@ class Notice(models.Model):
     title = models.CharField(max_length=500, null=True, blank=True, verbose_name='هدر')
     message = models.TextField(null=False, blank=False, verbose_name='متن اعلان')
     icon = models.CharField(null=True, blank=True, default='fa-bell', verbose_name='ایکون اعلان')
+    user = models.ManyToManyField(User, blank=True, verbose_name='برای کدام کاربران')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='آخرین آپدیت')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
     is_active = models.BooleanField(default=True, null=False, verbose_name='فعال / غیر فعال')
