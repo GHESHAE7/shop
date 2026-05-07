70fbf52682e50c6490ea79d0f558d716815661cb
farhan <farhan.azizi.gh@gmail.com>
2026-05-07 13:15:22 +0330

    feat: added PrivacyPolicy model to admin.py

--- about_module/admin.py
+++ about_module/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-from .models import About
+from .models import About, PrivacyPolicy
 # Register your models here.
 
 
 @admin.register(About)
 class AboutAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active', 'updated_at', 'created_at',)
+    list_filter = ('created_at', 'updated_at', 'is_active', 'name')
+    list_editable = ('is_active',)
+    readonly_fields = ('updated_at', 'created_at',)
+    
+    
+    
+@admin.register(PrivacyPolicy)
+class PrivacyPolicyAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_active', 'updated_at', 'created_at',)
     list_filter = ('created_at', 'updated_at', 'is_active', 'name')
     list_editable = ('is_active',)
