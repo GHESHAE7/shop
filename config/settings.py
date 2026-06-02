b719eb37b1a67844858c9cd586e7d4cd77d929d8
farhan <farhan.azizi.gh@gmail.com>
2026-06-02 11:27:55 +0330

    feat: added sites framework for project
    then, "python manage.py migrate"

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.humanize",
+    "django.contrib.sites",
     # package
     "django_render_partial",
     "captcha",
